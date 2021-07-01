07207f9914cbc5f1b7113e78cbcf930a552f3bc9
Ted M. Young <tedyoung@gmail.com>
2021-06-30 21:26:04 -0700

    Implemented addMoney() with balance()

--- src/test/java/com/jitterted/ebp/blackjack/WalletTest.java
+++ src/test/java/com/jitterted/ebp/blackjack/WalletTest.java
@@ -24,4 +24,34 @@ public class WalletTest {
                 .isFalse();
     }
 
+    @Test
+    public void newWalletHasZeroBalance() throws Exception {
+        Wallet wallet = new Wallet();
+
+        assertThat(wallet.balance())
+                .isZero();
+    }
+
+    @Test
+    public void newWalletAdd10ThenBalanceIs10() throws Exception {
+        Wallet wallet = new Wallet();
+
+        wallet.addMoney(10);
+
+        assertThat(wallet.balance())
+                .isEqualTo(10);
+    }
+
+    @Test
+    public void newWalletAdd7AndAdd8ThenBalanceIs15() throws Exception {
+        Wallet wallet = new Wallet();
+
+        wallet.addMoney(7);
+        wallet.addMoney(8);
+
+        assertThat(wallet.balance())
+                .isEqualTo(7 + 8);
+    }
+
+
 }
